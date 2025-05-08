7dd8c1e95c7e66921290aedd2e2126e4b427b361
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2025-05-08 11:41:42 -0300

    refactor: sou burro

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,19 +9,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <link
-          rel="preload"
-          href="/path/to/font.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/path/to/icons.svg"
-          as="image"
-          type="image/svg+xml"
-        />
       </head>
       <body>{children}</body>
     </html>
